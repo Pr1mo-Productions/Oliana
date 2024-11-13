51fc1aa2c39bf806e6ccb59fec1bd2dc8106b540
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-11-12 20:42:21 -0500

    Unfinished partial progress towards integrating this into our own runtime - https://github.com/Gadersd/stable-diffusion-burn/blob/main/src/bin/sample/main.rs

--- src/main.rs
+++ src/main.rs
@@ -41,6 +41,8 @@ async fn main() {
 
     // Image stuff
     let image_start = std::time::Instant::now();
+
+    /*
     let model = Wuerstchen::builder()
         //.with_flash_attn(true) // reduce GPU vram required - requires kalosm to expise a feature flag!
         .build().await.unwrap();
@@ -59,7 +61,32 @@ async fn main() {
                 eprintln!("Saved {}", &file);
             }
         }
+    }*/
+
+    if !std::path::Path::new("SDv1-4.mpk").exists() {
+        // Download it!
+        let mut downloader = downloader::Downloader::builder()
+            .download_folder(std::path::Path::new("."))
+            .parallel_requests(2)
+            .build()
+            .unwrap();
+
+        let dl = downloader::Download::new("https://huggingface.co/Gadersd/Stable-Diffusion-Burn/resolve/main/SDv1-4.mpk");
+
+        let result = downloader.async_download(&[dl]).await.unwrap();
+
+        for r in result {
+            match r {
+                Err(e) => println!("Error: {}", e.to_string()),
+                Ok(s) => println!("Success: {}", &s),
+            };
+        }
     }
+
+    // stablediffusion::
+
+
+
     let image_end = std::time::Instant::now();
 
     println!("Total Time: {}", utils::duration_to_display_str(&(image_end - main_start)));
@@ -74,3 +101,117 @@ async fn main() {
 
 
 
+use stablediffusion::{
+    model::stablediffusion::{load::load_stable_diffusion, *},
+    tokenizer::SimpleTokenizer,
+};
+
+use burn::{
+    config::Config,
+    module::{Module, Param},
+    nn,
+    tensor::{backend::Backend, Tensor},
+};
+
+        use burn_tch::{LibTorch, LibTorchDevice};
+
+use std::env;
+use std::io;
+use std::process;
+
+use burn::record::{self, NamedMpkFileRecorder, FullPrecisionSettings, Recorder};
+
+fn load_stable_diffusion_model_file<B: Backend>(
+    filename: &str,
+    device: &B::Device,
+) -> Result<StableDiffusion<B>, record::RecorderError> {
+    NamedMpkFileRecorder::<FullPrecisionSettings>::new()
+        .load(filename.into(), device)
+        .map(|record| StableDiffusionConfig::new().init(device).load_record(record))
+}
+
+fn not_main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 7 && args.len() != 8 {
+        eprintln!("Usage: {} <model_type(burn or dump)> <model_name> <unconditional_guidance_scale> <n_diffusion_steps> <prompt> <output_image_name> [device(cuda, mps, cpu)]", args[0]);
+        process::exit(1);
+    }
+
+    let model_type = &args[1];
+    let model_name = &args[2];
+    let unconditional_guidance_scale: f64 = args[3].parse().unwrap_or_else(|_| {
+        eprintln!("Error: Invalid unconditional guidance scale.");
+        process::exit(1);
+    });
+    let n_steps: usize = args[4].parse().unwrap_or_else(|_| {
+        eprintln!("Error: Invalid number of diffusion steps.");
+        process::exit(1);
+    });
+    let prompt = &args[5];
+    let output_image_name = &args[6];
+
+    // Optional device parameter
+    let device_arg = if args.len() == 8 { Some(&args[7]) } else { None };
+
+
+    type Backend = LibTorch<f32>;
+
+    let device = if let Some(dev_str) = device_arg {
+        match dev_str.to_lowercase().as_str() {
+            "cpu" => LibTorchDevice::Cpu,
+            "mps" => LibTorchDevice::Mps,
+            s if s.starts_with("cuda") => {
+                let idx = s[4..].parse().unwrap_or(0);
+                LibTorchDevice::Cuda(idx)
+            }
+            _ => {
+                eprintln!("Unknown device: {}", dev_str);
+                process::exit(1);
+            }
+        }
+    } else {
+        LibTorchDevice::Cuda(0)
+    };
+
+    println!("Loading tokenizer...");
+    let tokenizer = SimpleTokenizer::new().unwrap();
+    println!("Loading model...");
+    let sd: StableDiffusion<Backend> = if model_type == "burn" {
+        load_stable_diffusion_model_file(model_name, &device).unwrap_or_else(|err| {
+            eprintln!("Error loading model: {}", err);
+            process::exit(1);
+        })
+    } else {
+        load_stable_diffusion(model_name, &device).unwrap_or_else(|err| {
+            eprintln!("Error loading model dump: {}", err);
+            process::exit(1);
+        })
+    };
+
+    let unconditional_context = sd.unconditional_context(&tokenizer);
+    let context = sd.context(&tokenizer, prompt).unsqueeze::<3>(); //.repeat(0, 2); // generate 2 samples
+
+    println!("Sampling image...");
+    let images = sd.sample_image(
+        context,
+        unconditional_context,
+        unconditional_guidance_scale,
+        n_steps,
+    );
+    save_images(&images, output_image_name, 512, 512).unwrap_or_else(|err| {
+        eprintln!("Error saving image: {}", err);
+        process::exit(1);
+    });
+}
+
+use image;
+use image::{ColorType::Rgb8, ImageResult};
+
+fn save_images(images: &Vec<Vec<u8>>, basepath: &str, width: u32, height: u32) -> ImageResult<()> {
+    for (index, img_data) in images.iter().enumerate() {
+        let path = format!("{}{}.png", basepath, index);
+        image::save_buffer(path, &img_data[..], width, height, Rgb8)?;
+    }
+
+    Ok(())
+}
