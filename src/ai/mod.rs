ca676d5a3b64f0d531adc28382e9d7c01aa7264f
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-11-23 15:51:08 -0500

    added some download management, cool progress bars

--- src/ai/mod.rs
+++ src/ai/mod.rs
@@ -6,7 +6,34 @@
 // offers a uniform input for many types of models and allows fairly transparent runtime
 // selection of GPU, CPU, and NPU compute devices
 
-fn load_ort_session() -> Result<ort::session::Session, Box<dyn std::error::Error>> {
+pub async fn load_ort_session(
+  local_onnx_file_path: impl Into<std::path::PathBuf>,
+  remote_onnx_download_url: &str
+) -> Result<ort::session::Session, Box<dyn std::error::Error>> {
+  let local_onnx_file_path: std::path::PathBuf = local_onnx_file_path.into();
+
+  if !tokio::fs::try_exists(&local_onnx_file_path).await? {
+    eprintln!("Downloading {} to {}", remote_onnx_download_url, &local_onnx_file_path.to_string_lossy() );
+    let mut downloader = downloader::Downloader::builder()
+          .download_folder( local_onnx_file_path.parent().ok_or_else(|| return "No Parent Directory for passed file to be downloaded!" ).map_err(crate::utils::eloc!())? )
+          .parallel_requests(2)
+          .build()?;
+    let dl_file_name_osstr = local_onnx_file_path.file_name().ok_or_else(|| return "No File Name for passed file to be downloaded!" ).map_err(crate::utils::eloc!())?;
+    let dl_file_name_string = dl_file_name_osstr.to_string_lossy().into_owned();
+
+    let dl = downloader::Download::new(remote_onnx_download_url)
+                .file_name( &std::path::Path::new( &dl_file_name_string ) )
+                .progress(std::sync::Arc::new(
+                  crate::utils::DownloadProgressReporter::new()
+                ));
+
+    let result = downloader.async_download(&[dl]).await?;
+
+  }
+  else {
+    eprintln!("Found already-downloaded file {}", &local_onnx_file_path.to_string_lossy() );
+  }
+
 
   let environment = ort::environment::Environment::builder()
     .with_name("test")
@@ -17,17 +44,20 @@ fn load_ort_session() -> Result<ort::session::Session, Box<dyn std::error::Error
   let mut session = ort::session::SessionBuilder::new(&environment)?
     .with_optimization_level(ort::GraphOptimizationLevel::Level1)?
     .with_intra_threads(1)?
-    .with_model_from_file("squeezenet.onnx").map_err(crate::utils::eloc!())?;
+    .with_model_from_file(local_onnx_file_path).map_err(crate::utils::eloc!())?;
 
   Ok(session)
 }
 
 
 
-pub fn get_compute_device_names() -> Result<Vec<String>, Box<dyn std::error::Error>> {
+pub async fn get_compute_device_names() -> Result<Vec<String>, Box<dyn std::error::Error>> {
   let mut compute_device_names: Vec<String> = vec![];
 
-  let ort_session = load_ort_session()?;
+  let ort_session = load_ort_session(
+    crate::utils::get_cache_file("gpt2.onnx").await?,
+    "https://parcel.pyke.io/v2/cdn/assetdelivery/ortrsv2/ex_models/gpt2.onnx"
+  ).await?;
 
 
 /*
@@ -59,7 +89,7 @@ pub fn get_compute_device_names() -> Result<Vec<String>, Box<dyn std::error::Err
   Ok(compute_device_names)
 }
 
-pub fn run_oneshot_llm_prompt(prompt_txt: &str) -> Result<String, Box<dyn std::error::Error>> {
+pub async fn run_oneshot_llm_prompt(prompt_txt: &str) -> Result<String, Box<dyn std::error::Error>> {
   let mut reply = String::new();
 
 
@@ -70,7 +100,7 @@ pub fn run_oneshot_llm_prompt(prompt_txt: &str) -> Result<String, Box<dyn std::e
 
 
 
-pub fn run_oneshot_ai_img_prompt(prompt_txt: &str, out_file_path: &str) -> Result<String, Box<dyn std::error::Error>> {
+pub async fn run_oneshot_ai_img_prompt(prompt_txt: &str, out_file_path: &str) -> Result<String, Box<dyn std::error::Error>> {
 
 
 
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -59,3 +59,62 @@ macro_rules! eloc {
 pub(crate) use eloc;
 
 
+pub async fn get_cache_dir() -> Result<std::path::PathBuf, Box<dyn std::error::Error>> {
+  let mut user_cache_path = dirs::cache_dir().ok_or_else(|| return "No Cache Directory on this operating system!" ).map_err(crate::utils::eloc!())?;
+  user_cache_path.push(env!("CARGO_PKG_NAME"));
+  tokio::fs::create_dir_all(&user_cache_path).await?;
+  Ok(user_cache_path)
+}
+
+pub async fn get_cache_file(file_name: &str) -> Result<std::path::PathBuf, Box<dyn std::error::Error>> {
+    let mut pb = get_cache_dir().await?;
+    pb.push(file_name);
+    Ok(pb)
+}
+
+
+pub struct DownloadProgressReporter {
+    pub max_progress: std::cell::UnsafeCell<std::option::Option<u64>>,
+    pub bar: indicatif::ProgressBar,
+}
+
+unsafe impl Sync for DownloadProgressReporter { } // Because I said so, our UnsafeCell is just a number in memory
+
+impl DownloadProgressReporter {
+    pub fn new() -> Self {
+        Self {
+            max_progress: None.into(),
+            bar: indicatif::ProgressBar::no_length()
+        }
+    }
+}
+
+impl Drop for DownloadProgressReporter {
+    fn drop(&mut self) {
+        self.bar.finish();
+    }
+}
+
+
+impl downloader::progress::Reporter for DownloadProgressReporter {
+    fn setup(&self, max_progress: std::option::Option<u64>, message: &str) {
+        unsafe { *self.max_progress.get() = max_progress.into(); } // Assigns into a read-only reference; safe because I say the compiler won't optimize through an UnsafeCell
+        if let Some(max_progress_val) = max_progress {
+            self.bar.set_length(max_progress_val);
+        }
+    }
+    fn progress(&self, current: u64) {
+        if current > self.bar.position() {
+            let incr_amnt = current - self.bar.position();
+            self.bar.inc(incr_amnt);
+        }
+    }
+    fn set_message(&self, message: &str) {
+
+    }
+    fn done(&self) {
+        self.bar.finish();
+    }
+}
+
+
