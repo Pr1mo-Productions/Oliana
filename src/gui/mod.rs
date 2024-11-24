c940365e1fa98439508c8eb516e80c58b8173feb
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-11-24 15:38:20 -0500

    added some primitive text handling from bevy_simple_text_input!

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -6,6 +6,15 @@ use bevy::{
     window::{CursorGrabMode, PresentMode, WindowLevel, WindowTheme},
 };
 
+use bevy_simple_text_input::{
+    TextInputBundle, TextInputInactive, TextInputPlugin, TextInputSystem, TextInputSubmitEvent
+};
+
+const BORDER_COLOR_ACTIVE: Color = Color::srgb(0.75, 0.52, 0.99);
+const BORDER_COLOR_INACTIVE: Color = Color::srgb(0.25, 0.25, 0.25);
+const TEXT_COLOR: Color = Color::srgb(0.9, 0.9, 0.9);
+const BACKGROUND_COLOR: Color = Color::srgb(0.15, 0.15, 0.15);
+
 pub async fn open_gui_window() -> Result<(), Box<dyn std::error::Error>>  {
   App::new()
     .add_plugins((
@@ -44,6 +53,10 @@ pub async fn open_gui_window() -> Result<(), Box<dyn std::error::Error>>  {
                 make_visible,
             ),
         )
+        .add_plugins(TextInputPlugin)
+        .add_systems(Startup, setup)
+        .add_systems(Update, focus.before(TextInputSystem))
+        .add_systems(Update, text_listener.after(TextInputSystem))
        .run();
 
    Ok(())
@@ -182,4 +195,77 @@ fn cycle_cursor_icon(
     }
 
     window.cursor.icon = ICONS[*index];
+}
+
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+
+    commands
+        .spawn((
+            NodeBundle {
+                style: Style {
+                    width: Val::Percent(100.0),
+                    height: Val::Percent(100.0),
+                    align_items: AlignItems::Center,
+                    justify_content: JustifyContent::Center,
+                    ..default()
+                },
+                ..default()
+            },
+            // Make this container node bundle to be Interactive so that clicking on it removes
+            // focus from the text input.
+            Interaction::None,
+        ))
+        .with_children(|parent| {
+            parent.spawn((
+                NodeBundle {
+                    style: Style {
+                        width: Val::Px(400.0),
+                        border: UiRect::all(Val::Px(5.0)),
+                        padding: UiRect::all(Val::Px(5.0)),
+                        ..default()
+                    },
+                    border_color: BORDER_COLOR_INACTIVE.into(),
+                    background_color: BACKGROUND_COLOR.into(),
+                    // Prevent clicks on the input from also bubbling down to the container
+                    // behind it
+                    focus_policy: bevy::ui::FocusPolicy::Block,
+                    ..default()
+                },
+                TextInputBundle::default()
+                    .with_text_style(TextStyle {
+                        font_size: 40.,
+                        color: TEXT_COLOR,
+                        ..default()
+                    })
+                    .with_placeholder("Click Me", None)
+                    .with_inactive(true),
+            ));
+        });
+}
+
+fn focus(
+    query: Query<(Entity, &Interaction), Changed<Interaction>>,
+    mut text_input_query: Query<(Entity, &mut TextInputInactive, &mut BorderColor)>,
+) {
+    for (interaction_entity, interaction) in &query {
+        if *interaction == Interaction::Pressed {
+            for (entity, mut inactive, mut border_color) in &mut text_input_query {
+                if entity == interaction_entity {
+                    inactive.0 = false;
+                    *border_color = BORDER_COLOR_ACTIVE.into();
+                } else {
+                    inactive.0 = true;
+                    *border_color = BORDER_COLOR_INACTIVE.into();
+                }
+            }
+        }
+    }
+}
+
+fn text_listener(mut events: EventReader<TextInputSubmitEvent>) {
+    for event in events.read() {
+        info!("{:?} submitted: {}", event.entity, event.value);
+    }
 }
