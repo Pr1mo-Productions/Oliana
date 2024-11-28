784dbea0515f2f2497b811f80a69215934058ff6
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-11-28 15:42:38 -0500

    Ok tiny milestone - we can keep the Ollama state reasonably tracked while the user types things in & then we an print ollama replies to the screen. TODO probably some word-wrapping based on the screen size.

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -10,11 +10,12 @@ use bevy_simple_text_input::{
     TextInputBundle, TextInputInactive, TextInputPlugin, TextInputSystem, TextInputSubmitEvent
 };
 
+use bevy_defer::AsyncCommandsExtension;
+
 const BORDER_COLOR_ACTIVE: Color = Color::srgb(0.75, 0.52, 0.99);
 const BORDER_COLOR_INACTIVE: Color = Color::srgb(0.25, 0.25, 0.25);
 const TEXT_COLOR: Color = Color::srgb(0.9, 0.9, 0.9);
 const BACKGROUND_COLOR: Color = Color::srgb(0.15, 0.15, 0.15);
-
 
 
 pub async fn open_gui_window(cli_args: &crate::cli::Args) -> Result<(), Box<dyn std::error::Error>> {
@@ -43,24 +44,28 @@ pub async fn open_gui_window(cli_args: &crate::cli::Args) -> Result<(), Box<dyn 
         FrameTimeDiagnosticsPlugin,
     ))
     .add_plugins(bevy_defer::AsyncPlugin::default_settings())
+    .add_plugins(TextInputPlugin)
+
+    .add_event::<OllamaIsReadyToProcessEvent>()
+    .add_event::<PromptToOllamaEvent>()
+    .add_event::<ResponseFromOllamaEvent>()
+
+    .insert_resource((*cli_args).clone()) // Accept a Ref<crate::cli::Args> in your system's function to read cli args in the UI
+    .insert_resource(OllamaResource::default()) // Accept a Ref<crate::gui::OllamaResource> in your system's function to touch the Ollama stuff
+
     .add_systems(
         Update,
         (
-            change_title,
-            toggle_theme,
-            //toggle_cursor,
-            toggle_vsync,
-            toggle_window_controls,
-            switch_level,
             make_visible,
         ),
     )
-    .add_plugins(TextInputPlugin)
-    .add_systems(Startup, setup)
-    .insert_resource((*cli_args).clone())
-    .add_systems(Startup, create_ai_engine)
+    .add_systems(Startup, (setup, setup_ollama) )
     .add_systems(Update, focus.before(TextInputSystem))
     .add_systems(Update, text_listener.after(TextInputSystem))
+    .add_systems(Update, read_ollama_ready_events)
+    .add_systems(Update, read_ollama_response_events)
+    .add_systems(Update, read_ollama_prompt_events)
+
    .run();
 
    Ok(())
@@ -75,105 +80,6 @@ fn make_visible(mut window: Query<&mut Window>, frames: Res<FrameCount>) {
         window.single_mut().visible = true;
     }
 }
-
-/// This system toggles the vsync mode when pressing the button V.
-/// You'll see fps increase displayed in the console.
-fn toggle_vsync(input: Res<ButtonInput<KeyCode>>, mut windows: Query<&mut Window>) {
-    if input.just_pressed(KeyCode::KeyV) {
-        let mut window = windows.single_mut();
-
-        window.present_mode = if matches!(window.present_mode, PresentMode::AutoVsync) {
-            PresentMode::AutoNoVsync
-        } else {
-            PresentMode::AutoVsync
-        };
-        info!("PRESENT_MODE: {:?}", window.present_mode);
-    }
-}
-
-/// This system switches the window level when pressing the T button
-/// You'll notice it won't be covered by other windows, or will be covered by all the other
-/// windows depending on the level.
-///
-/// This feature only works on some platforms. Please check the
-/// [documentation](https://docs.rs/bevy/latest/bevy/prelude/struct.Window.html#structfield.window_level)
-/// for more details.
-
-fn switch_level(input: Res<ButtonInput<KeyCode>>, mut windows: Query<&mut Window>) {
-    if input.just_pressed(KeyCode::KeyT) {
-        let mut window = windows.single_mut();
-
-        window.window_level = match window.window_level {
-            WindowLevel::AlwaysOnBottom => WindowLevel::Normal,
-            WindowLevel::Normal => WindowLevel::AlwaysOnTop,
-            WindowLevel::AlwaysOnTop => WindowLevel::AlwaysOnBottom,
-        };
-        info!("WINDOW_LEVEL: {:?}", window.window_level);
-    }
-}
-
-/// This system toggles the window controls when pressing buttons 1, 2 and 3
-///
-/// This feature only works on some platforms. Please check the
-/// [documentation](https://docs.rs/bevy/latest/bevy/prelude/struct.Window.html#structfield.enabled_buttons)
-/// for more details.
-fn toggle_window_controls(input: Res<ButtonInput<KeyCode>>, mut windows: Query<&mut Window>) {
-    let toggle_minimize = input.just_pressed(KeyCode::Digit1);
-    let toggle_maximize = input.just_pressed(KeyCode::Digit2);
-    let toggle_close = input.just_pressed(KeyCode::Digit3);
-
-    if toggle_minimize || toggle_maximize || toggle_close {
-        let mut window = windows.single_mut();
-
-        if toggle_minimize {
-            window.enabled_buttons.minimize = !window.enabled_buttons.minimize;
-        }
-        if toggle_maximize {
-            window.enabled_buttons.maximize = !window.enabled_buttons.maximize;
-        }
-        if toggle_close {
-            window.enabled_buttons.close = !window.enabled_buttons.close;
-        }
-    }
-}
-
-/// This system will then change the title during execution
-fn change_title(mut windows: Query<&mut Window>, time: Res<Time>) {
-    let mut window = windows.single_mut();
-    window.title = format!(
-        "Seconds since startup: {}",
-        time.elapsed().as_secs_f32().round()
-    );
-}
-
-/* // Useful - this is how we lock the cursor if we ever want WASD-style movement mechanics w/ cursor for camera control
-fn toggle_cursor(mut windows: Query<&mut Window>, input: Res<ButtonInput<KeyCode>>) {
-    if input.just_pressed(KeyCode::Space) {
-        let mut window = windows.single_mut();
-
-        window.cursor.visible = !window.cursor.visible;
-        window.cursor.grab_mode = match window.cursor.grab_mode {
-            CursorGrabMode::None => CursorGrabMode::Locked,
-            CursorGrabMode::Locked | CursorGrabMode::Confined => CursorGrabMode::None,
-        };
-    }
-}*/
-
-
-// This system will toggle the color theme used by the window
-fn toggle_theme(mut windows: Query<&mut Window>, input: Res<ButtonInput<KeyCode>>) {
-    if input.just_pressed(KeyCode::KeyF) {
-        let mut window = windows.single_mut();
-
-        if let Some(current_theme) = window.window_theme {
-            window.window_theme = match current_theme {
-                WindowTheme::Light => Some(WindowTheme::Dark),
-                WindowTheme::Dark => Some(WindowTheme::Light),
-            };
-        }
-    }
-}
-
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
@@ -222,7 +128,7 @@ fn setup(mut commands: Commands) {
             ));
         });
 
-    // Text with one section
+    // Text with one section; OllamaReplyText allows us to refer to the TextBundle?
     commands.spawn((
         // Create a TextBundle that has a Text with a single section.
         TextBundle::from_section(
@@ -243,11 +149,8 @@ fn setup(mut commands: Commands) {
             left: Val::Px(4.0),
             ..default()
         }),
-        //ColorText,
+        OllamaReplyText,
     ));
-
-
-
 
 }
 
@@ -270,127 +173,139 @@ fn focus(
     }
 }
 
-fn text_listener(mut events: EventReader<TextInputSubmitEvent>, ) {
+fn text_listener(mut events: EventReader<TextInputSubmitEvent>, mut event_writer: EventWriter<PromptToOllamaEvent>,) {
     for event in events.read() {
         info!("{:?} submitted: {}", event.entity, event.value);
+        event_writer.send(PromptToOllamaEvent(event.value.clone()));
     }
 }
 
-fn create_ai_engine(cli_args: Res<crate::cli::Args>, mut commands: Commands) {
-    use bevy_defer::AsyncCommandsExtension;
-    //let handle = tokio::runtime::Handle::current(); // Safety: Bevy runs in one of tokio's async methods, therefore MUST have an EnterGuard setup.
-    //let ai_engine = handle.block_on(AI_Engine::new(&cli_args));
-    //commands.spawn(ai_engine);
-    let cli_args_clone = cli_args.clone();
+fn read_ollama_ready_events(
+    mut event_reader: EventReader<OllamaIsReadyToProcessEvent>,
+) {
+    for ev in event_reader.read() {
+        eprintln!("Event {:?} recieved!", ev);
+    }
+}
+
+fn read_ollama_response_events(
+    mut event_reader: EventReader<ResponseFromOllamaEvent>,
+    mut query: Query<&mut Text, With<OllamaReplyText>>
+) {
+    for ev in event_reader.read() {
+        eprintln!("Event {:?} recieved!", ev);
+        for mut text in &mut query { // We'll only ever have 1 section of text rendered
+            text.sections[0].value = ev.0.to_string();
+        }
+    }
+}
+
+fn read_ollama_prompt_events(
+    mut commands: Commands,
+    mut event_reader: EventReader<PromptToOllamaEvent>,
+    // mut event_writer: EventWriter<ResponseFromOllamaEvent>,
+    mut ollama_resource: ResMut<crate::gui::OllamaResource>,
+) {
+    let arc_to_ollama_rwlock = ollama_resource.into_inner().ollama_inst.clone();
+    for ev in event_reader.read() {
+        let ev_txt = ev.0.to_string();
+        eprintln!("Passing this prompt to Ollama: {:?}", ev.0);
+
+        let closure_arc_to_ollama_rwlock = arc_to_ollama_rwlock.clone();
+
+        commands.spawn_task(|| async move {
+            let ollama_resource_readlock = std::sync::RwLock::read(&closure_arc_to_ollama_rwlock).expect("Could not get read-only access to Ollama instance!");
+
+            let ollama_resp = ollama_resource_readlock.generate(ollama_rs::generation::completion::request::GenerationRequest::new("qwen2.5:7b".to_string(), ev_txt)).await;
+
+            match ollama_resp {
+                Ok(resp) => {
+                    eprintln!("resp.response = {:?}", resp.response);
+                    bevy_defer::access::AsyncWorld.send_event(ResponseFromOllamaEvent(resp.response.to_string())).expect("async error");
+                }
+                Err(e) => {
+                    eprintln!("resp e = {:?}", e);
+                    bevy_defer::access::AsyncWorld.send_event(ResponseFromOllamaEvent(format!("{:?}", e))).expect("async error");
+                }
+            }
+
+
+            Ok(())
+        });/*
+        .add(move |w: &mut World| {
+            // Poll for a reply up to 90 seconds; forwarding first non-empty value we unpack
+            //let resp_str = sync_boxed_reply.read().expect("Cannot aquire read lock!").to_string();
+            //w.send_event(ResponseFromOllamaEvent(resp_str));
+            let mut remaining_polls: usize = 90 * 4;
+            loop {
+                if remaining_polls < 1 {
+                    eprintln!("TIMED OUT Waiting for Ollama reply!");
+                    break;
+                }
+                remaining_polls -= 1;
+
+                match sync_boxed_reply.try_read() {
+                    Ok(resp_str) => {
+                        if resp_str.len() > 0 {
+                            w.send_event(ResponseFromOllamaEvent(resp_str.to_string()));
+                            break;
+                        }
+                        else {
+                            // Read success, but no data rx-ed yet so we fall through
+                        }
+                    }
+                    Err(e) => {
+                        // Fall-through
+                    }
+                };
+
+                // No data rx-ed yet OR error recieving so we wait
+                std::thread::sleep(std::time::Duration::from_millis(250));
+            }
+
+        });*/
+
+    }
+}
+
+
+fn setup_ollama(mut commands: Commands, mut ollama_resource: ResMut<crate::gui::OllamaResource>, cli_args: Res<crate::cli::Args>, mut ev_ollama_ready: EventWriter<OllamaIsReadyToProcessEvent>) {
+
+    // The write lock is NOT dropped here, it is MOVED into the async context below.
+    let owned_cli_args: crate::cli::Args = cli_args.clone();
+    let arc_to_ollama_rwlock = ollama_resource.into_inner().ollama_inst.clone();
+
     commands.spawn_task(|| async move {
-        let ai_engine = AI_Engine::new(&cli_args_clone).await;
-        //commands.spawn(ai_engine); // TODO hmmmmm
+
+        let mut ollama_resource_writelock = std::sync::RwLock::write(&arc_to_ollama_rwlock).expect("Cannot get Write lock of OllamaResource.ollama_inst");
+        *ollama_resource_writelock = crate::ai::init_ollama_with_model_pulled(&owned_cli_args, "qwen2.5:7b").await.unwrap();
+
+        // std::thread::sleep(std::time::Duration::from_millis(3500)); // Haha yeah we suck, but this is a good knee-jerk measurement of ^^ lotsa work upstairs
+
         Ok(())
+    })
+    .add(|w: &mut World| {
+        w.send_event(OllamaIsReadyToProcessEvent());
     });
 }
 
-const OLLAMA_MODEL_NAME: &'static str = "qwen2.5:7b";
 
+
+#[derive(Debug, Clone, Default, bevy::ecs::system::Resource)]
+pub struct OllamaResource {
+    pub ollama_inst: std::sync::Arc<std::sync::RwLock<ollama_rs::Ollama>>,
+}
+
+#[derive(Debug, bevy::ecs::event::Event)]
+pub struct OllamaIsReadyToProcessEvent();
+
+#[derive(Debug, bevy::ecs::event::Event)]
+pub struct PromptToOllamaEvent(String);
+
+#[derive(Debug, bevy::ecs::event::Event)]
+pub struct ResponseFromOllamaEvent(String);
+
+
+// A unit struct to help identify the Ollama Reply UI component, since there may be many Text components
 #[derive(Component)]
-struct AI_Engine {
-    pub ollama_inst: ollama_rs::Ollama,
-    pub have_verified_qwen2_5_7b_up: bool,
-    pub streaming_reply_tokens: std::sync::Arc<std::sync::RwLock<Vec<String>>>,
-}
-
-impl AI_Engine {
-    pub async fn new(cli_args: &crate::cli::Args) -> Self {
-        let mut s = Self {
-            ollama_inst: crate::ai::init_ollama_with_model_pulled(cli_args, "qwen2.5:7b").await.expect("Could not run ollama, ensure ollama[.exe] is installed an on PATH!"),
-            have_verified_qwen2_5_7b_up: false,
-            streaming_reply_tokens: std::sync::Arc::new(vec![].into()),
-            //self_ref: None,
-        };
-        //s.self_ref = Some(std::sync::Arc::new(s));
-        s
-    }
-
-    pub fn begin_prompting(&mut self, prompt_txt: &str) {
-        let handle = tokio::runtime::Handle::current(); // Safety: Bevy runs in one of tokio's async methods, therefore MUST have an EnterGuard setup.
-
-        handle.block_on(self.ensure_qwen2_5_7b_up());
-
-        let ollama_dupe_client = self.ollama_inst.clone();
-        let prompt_txt_one = prompt_txt.to_string();
-        let streaming_reply_tokens_jh = self.streaming_reply_tokens.clone();
-        let join_handle = handle.spawn({
-            async move {
-                match ollama_dupe_client.generate(ollama_rs::generation::completion::request::GenerationRequest::new(OLLAMA_MODEL_NAME.to_string(), prompt_txt_one)).await {
-                    Ok(res) => {
-                        streaming_reply_tokens_jh.write().expect("No writable ref available").push(res.response);
-                    }
-                    Err(e) => {
-                        eprintln!("{:?}", e);
-                        streaming_reply_tokens_jh.write().expect("No writable ref available").push(format!("{:?}", e));
-                    }
-                }
-            }
-        });
-
-    }
-
-    pub async fn ensure_qwen2_5_7b_up(&mut self) {
-        if self.have_verified_qwen2_5_7b_up {
-            return;
-        }
-        if let Err(e) = self.ensure_qwen2_5_7b_up_errs().await {
-            eprintln!("{:?}", e);
-        }
-    }
-    pub async fn ensure_qwen2_5_7b_up_errs(&mut self) -> Result<(), Box<dyn std::error::Error>>  {
-        match self.ollama_inst.list_local_models().await {
-          Ok(local_models) => {
-            /* Nop */
-          }
-          Err(e) => {
-            eprintln!("{:#?}", crate::utils::LocatedError { inner: Box::new(e), file: file!(), line: line!(), column: column!(), addtl_msg: String::new() });
-
-            eprintln!("Executing 'ollama serve' as a background process...");
-
-            tokio::process::Command::new("ollama")
-              .args(&["serve"])
-              .kill_on_drop(false) // Prevents tokio from reaping process on Drop
-              .spawn().map_err(crate::utils::eloc!())?;
-
-            // Delay for 750ms or so
-            tokio::time::sleep(std::time::Duration::from_millis(750)).await;
-          }
-        }
-
-        let local_models = self.ollama_inst.list_local_models().await.map_err(crate::utils::eloc!())?;
-        // eprintln!("Ollama models = {:#?}", local_models);
-
-        /*let qwen2_5_7b_model_file = download_file_ifne(
-          cli_args,
-          crate::utils::get_cache_file("qwen2_5_7b.Modelfile").await?,
-          "https://huggingface.co/openai-community/gpt2/raw/main/tokenizer.json"
-        ).await?;*/
-        // ^^ todo research so we can control our own downloads
-
-        match self.ollama_inst.show_model_info(OLLAMA_MODEL_NAME.to_string()).await {
-          Ok(model_info) => { /* unused */ },
-          Err(e) => {
-            eprintln!("{:#?}", crate::utils::LocatedError { inner: Box::new(e), file: file!(), line: line!(), column: column!(), addtl_msg: String::new() });
-            // Spawn off a download
-            eprintln!("Telling ollama to pull the model {}...", OLLAMA_MODEL_NAME);
-            self.ollama_inst.pull_model(OLLAMA_MODEL_NAME.to_string(), true).await?;
-            eprintln!("Done pulling {}!", OLLAMA_MODEL_NAME);
-          }
-        }
-
-        self.have_verified_qwen2_5_7b_up = true;
-
-        Ok(())
-    }
-}
-
-
-
-
-
-
+struct OllamaReplyText;
