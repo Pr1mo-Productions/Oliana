e3c0685d2512e2d9debaa669de9b6ae04c52fa5e
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-11-30 22:33:28 -0500

    Got the following to work kinda: LIBTORCH_USE_PYTORCH=1 PYTHONPATH=/j/proj/Oliana/utils/pyenv LD_LIBRARY_PATH=:/j/proj/Oliana/utils/pyenv/torch/lib cargo run --release -- -v

--- utils/lookup_torch_libraries.py
+++ utils/lookup_torch_libraries.py
@@ -12,7 +12,10 @@ try:
 except:
   subprocess.run([
     sys.executable, '-m', 'pip', 'install',
-      f'--target={pyenv}', 'torch', 'torchvision', 'torchaudio'
+      f'--target={pyenv}',
+      # 'torch', 'torchvision', 'torchaudio'
+      #'torch==2.5.0', 'torchvision==0.20.0', 'torchaudio==2.5.0', '--index-url', 'https://download.pytorch.org/whl/cu124'
+      'torch==2.2.0', 'torchvision==0.17.0', 'torchaudio==2.2.0', '--index-url', 'https://download.pytorch.org/whl/cu121'
   ])
   import torch
 
@@ -20,7 +23,7 @@ print(f'torch={torch}')
 print(f'torch.cuda.is_available() = {torch.cuda.is_available()}')
 first_cuda_device = torch.device('cuda')
 print(f'first_cuda_device = {first_cuda_device}')
-print(f'memory info of device = {torch.cuda.mem_get_info(first_cuda_device)}')
+print(f'memory info of device = {torch.cuda.mem_get_info()}')
 
 # The following lists _ALL_ open filed, including the copy of libtorch_cuda.so we've just loaded.
 subprocess.run([
