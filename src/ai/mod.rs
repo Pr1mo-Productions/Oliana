4f7c57211501edbb38197e3c2873057f9d502c62
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-12-01 13:59:01 -0500

    We crash in run_oneshot_ai_img_prompt, but it compiles!

--- src/ai/mod.rs
+++ src/ai/mod.rs
@@ -296,27 +296,33 @@ pub async fn run_oneshot_llm_prompt(cli_args: &crate::cli::Args, prompt_txt: &st
 
 
 pub async fn run_oneshot_ai_img_prompt(cli_args: &crate::cli::Args, prompt_txt: &str, out_file_path: &str) -> Result<String, Box<dyn std::error::Error>> {
+  use tch::nn::Module;
+  use tch::Kind;
 
   // First download all the models
   let local_clip_v2_1_ot = crate::utils::get_cache_file("rust-stable-diffusion-v2-1_clip_v2.1.ot").await?;
   let local_clip_v2_1_ot_path = download_file_ifne(
     cli_args, &local_clip_v2_1_ot, "https://huggingface.co/lmz/rust-stable-diffusion-v2-1/resolve/main/weights/clip_v2.1.ot"
   ).await?;
+  let local_clip_v2_1_ot_path_s = local_clip_v2_1_ot_path.to_string_lossy();
 
   let local_vae_v2_1_ot = crate::utils::get_cache_file("rust-stable-diffusion-v2-1_vae_v2.1.ot").await?;
   let local_vae_v2_1_ot_path = download_file_ifne(
     cli_args, &local_vae_v2_1_ot, "https://huggingface.co/lmz/rust-stable-diffusion-v2-1/resolve/main/weights/vae_v2.1.ot"
   ).await?;
+  let local_vae_v2_1_ot_path_s = local_vae_v2_1_ot_path.to_string_lossy();
 
   let local_unet_v2_1_ot = crate::utils::get_cache_file("rust-stable-diffusion-v2-1_unet_v2.1.ot").await?;
   let local_unet_v2_1_ot_path = download_file_ifne(
     cli_args, &local_unet_v2_1_ot, "https://huggingface.co/lmz/rust-stable-diffusion-v2-1/resolve/main/weights/unet_v2.1.ot"
   ).await?;
+  let local_unet_v2_1_ot_path_s = local_unet_v2_1_ot_path.to_string_lossy();
 
   let bpe_simple_vocab_16e6_txt = crate::utils::get_cache_file("rust-stable-diffusion-v2-1_bpe_simple_vocab_16e6.txt").await?;
   let bpe_simple_vocab_16e6_txt_path = download_file_ifne(
     cli_args, &bpe_simple_vocab_16e6_txt, "https://huggingface.co/lmz/rust-stable-diffusion-v2-1/raw/main/weights/bpe_simple_vocab_16e6.txt"
   ).await?;
+  let bpe_simple_vocab_16e6_txt_path_s = bpe_simple_vocab_16e6_txt_path.to_string_lossy();
 
   // tch::maybe_init_cuda(); // No longer exists in 0.18+
   tch::Cuda::cudnn_set_benchmark(false); // Doesn't work -_- https://github.com/LaurentMazare/diffusers-rs/issues/16#issuecomment-1376939427
@@ -325,10 +331,82 @@ pub async fn run_oneshot_ai_img_prompt(cli_args: &crate::cli::Args, prompt_txt: 
   eprintln!("Cudnn available: {}", tch::Cuda::cudnn_is_available());
   eprintln!("Cuda num devices: {}", tch::Cuda::device_count());
 
+  let n_steps: usize = 24;
+  let num_samples: i64 = 1;
+  let guidance_scale: f64 = 7.5;
+  let seed = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH)?.subsec_nanos() as i64;
 
+  let sd_config = diffusers::pipelines::stable_diffusion::StableDiffusionConfig::v2_1(None /* attn size */, Some(1024), Some(1024));
 
-  eprintln!("TODO!");
+  let device_setup = diffusers::utils::DeviceSetup::new(vec![]);
+  let clip_device = device_setup.get("clip");
+  let vae_device = device_setup.get("vae");
+  let unet_device = device_setup.get("unet");
+  let scheduler = sd_config.build_scheduler(n_steps);
 
+  let tokenizer = diffusers::transformers::clip::Tokenizer::create(bpe_simple_vocab_16e6_txt_path, &sd_config.clip)?;
+  println!("Running with prompt \"{prompt_txt}\".");
+  let tokens = tokenizer.encode(&prompt_txt)?;
+  let tokens: Vec<i64> = tokens.into_iter().map(|x| x as i64).collect();
+  let tokens = tch::Tensor::from_slice(&tokens).view((1, -1)).to(clip_device);
+  let uncond_tokens = tokenizer.encode("")?;
+  let uncond_tokens: Vec<i64> = uncond_tokens.into_iter().map(|x| x as i64).collect();
+  let uncond_tokens = tch::Tensor::from_slice(&uncond_tokens).view((1, -1)).to(clip_device);
+
+  let no_grad_guard = tch::no_grad_guard();
+
+  println!("Building the Clip transformer.");
+  let text_model = sd_config.build_clip_transformer(&local_clip_v2_1_ot_path_s, clip_device)?;
+  let text_embeddings = text_model.forward(&tokens);
+  let uncond_embeddings = text_model.forward(&uncond_tokens);
+  let text_embeddings = tch::Tensor::cat(&[uncond_embeddings, text_embeddings], 0).to(unet_device);
+
+  println!("Building the autoencoder.");
+  let vae = sd_config.build_vae(&local_vae_v2_1_ot_path_s, vae_device)?;
+  println!("Building the unet.");
+  let unet = sd_config.build_unet(&local_unet_v2_1_ot_path_s, unet_device, 4)?;
+
+  let bsize = 1;
+  for idx in 0..num_samples {
+      tch::manual_seed(seed + idx);
+      let mut latents = tch::Tensor::randn(
+          [bsize, 4, sd_config.height / 8, sd_config.width / 8],
+          (Kind::Float, unet_device),
+      );
+
+      // scale the initial noise by the standard deviation required by the scheduler
+      latents *= scheduler.init_noise_sigma();
+
+      for (timestep_index, &timestep) in scheduler.timesteps().iter().enumerate() {
+          println!("Timestep {timestep_index}/{n_steps}");
+          let latent_model_input = tch::Tensor::cat(&[&latents, &latents], 0);
+
+          let latent_model_input = scheduler.scale_model_input(latent_model_input, timestep);
+          let noise_pred = unet.forward(&latent_model_input, timestep as f64, &text_embeddings);
+          let noise_pred = noise_pred.chunk(2, 0);
+          let (noise_pred_uncond, noise_pred_text) = (&noise_pred[0], &noise_pred[1]);
+          let noise_pred =
+              noise_pred_uncond + (noise_pred_text - noise_pred_uncond) * guidance_scale;
+          latents = scheduler.step(&noise_pred, timestep, &latents);
+
+          /*if args.intermediary_images {
+              let latents = latents.to(vae_device);
+              let image = vae.decode(&(&latents / 0.18215));
+              let image = (image / 2 + 0.5).clamp(0., 1.).to_device(tch::Device::Cpu);
+              let image = (image * 255.).to_kind(Kind::Uint8);
+              let final_image =
+                  output_filename(&final_image, idx + 1, num_samples, Some(timestep_index + 1));
+              tch::vision::image::save(&image, final_image)?;
+          }*/
+      }
+
+      println!("Generating the final image for sample {}/{}.", idx + 1, num_samples);
+      let latents = latents.to(vae_device);
+      let image = vae.decode(&(&latents / 0.18215));
+      let image = (image / 2 + 0.5).clamp(0., 1.).to_device(tch::Device::Cpu);
+      let image = (image * 255.).to_kind(Kind::Uint8);
+      tch::vision::image::save(&image, out_file_path)?;
+  }
 
   Ok(out_file_path.to_string())
 }
