9e08b4ca95abaf1525a10f4598b1087c294d900a
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-12-30 18:09:50 -0500

    updates so anyone can run w/ the image experiment

--- Oliana-GUI/src/gui_setup.rs
+++ Oliana-GUI/src/gui_setup.rs
@@ -194,7 +194,7 @@ pub fn gui_setup(
             //Sprite::from_image(Image::transparent()),
             //Sprite::default()
             Sprite {
-                image: asset_server.load("/j/downloads/ai-cliffs-01.png"),
+                image: asset_server.load("../../docs/img/ai-cliffs-02.png"),
                 anchor: bevy::sprite::Anchor::Center,
                 image_mode: bevy::ui::prelude::SpriteImageMode::Auto,
                 custom_size: Some(bevy::math::f32::Vec2{x: 240.0, y:240.0}),
