4a89b34079161f297dcb445be8ceb3f2fb708913
Jeffrey McAteer <jeffrey@jmcateer.com>
2025-01-03 08:39:20 -0500

    fixed some feature issues and added an explicit script to compile w/ the best feature flags based on detected HW - in an installer we'll have to download the appropriate game for the box's hardware

---
+++ runscripts/jeff.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+# Yes, we swap compile commands based on plugged-in hardware.
+if lspci -k -d ::03xx | grep -i nvidia ; then
+  echo "Building with CUDA support"
+
+  cargo build --release \
+    --no-default-features \
+    --features oliana_gui/wayland \
+    --features oliana_images/cuda \
+    --features oliana_text/cuda
+
+  echo "Built with CUDA support!"
+else
+  echo "Building w/o CUDA support"
+
+  cargo build \
+    --release \
+    --no-default-features \
+    --features oliana_gui/wayland
+
+  echo "Built w/o CUDA support!"
+fi
+
+./target/release/oliana_gui || pkill oliana
+
